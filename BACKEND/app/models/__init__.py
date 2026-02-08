770ef0bc71fdd74172a5e70228f55fc455f1e26a
user_nekwasar <nekwasar@nekwasar.nekwasar.com>
2026-02-08 07:54:05 -0500

    Fix import errors in models/__init__.py

--- BACKEND/app/models/__init__.py
+++ BACKEND/app/models/__init__.py
@@ -1,7 +1,7 @@
 from .user import User, AdminUser
 from .blog import BlogPost, BlogComment, BlogLike, BlogView
 from .author import BlogAuthor
-from .contact import ContactRequests
-from .product import Product, ProductImage, ProductVideo
-from .store import StoreOrder, StoreOrderItem, StorePayment
+from .contact import ContactMessage
+from .product import LegacyProduct
+from .store import Product, ProductMedia, Order, OrderItem, Subscription
 from .media import FileAsset
